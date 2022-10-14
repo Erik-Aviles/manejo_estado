5e414929aacf7becb5b93e765c7f4e1b378e2355
Erika <erik8822@hotmail.com>
2022-10-13 21:26:15 -0500

    usanso UseReducer en todo su explendor (reduceObjet, reducer, actiocType)

--- src/components/UseState/index.jsx
+++ src/components/UseState/index.jsx
@@ -5,52 +5,124 @@ const SEGURITY_CODE = 'erikita';
 
 
 const UseState = ({name}) => {
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [value, setValue] = useState('');
+
+  const [state, setState] = useState({
+    value:'',
+    loading: false,
+    error: false,
+    deleted: false,
+    confirmed: false,
+
+  })
 
   const handleComprobar = () =>{
-    setLoading(true)
+    setState({
+      ...state, 
+      loading: true
+    })
   } 
 
   const onChange = (e) => {
-     setValue(e.target.value);
+    setState({
+      ...state, 
+      value: e.target.value
+    });
   }
 
   useEffect(() => {
-    if (loading) {
+    if (state.loading) {
       setTimeout(() => {
         console.log('Validando')
-        if (value !== SEGURITY_CODE) {
-          setError(true);
-          setLoading(false); 
+        if (state.value === SEGURITY_CODE) {
+          setState({
+            ...state, 
+            error: false,
+            loading: false, 
+            confirmed: true
+          });
         }else {
-          setError(false);
-          setLoading(false);
-          
+          setState({
+            ...state, 
+            error: true, 
+            loading: false,
+          });         
         }
         console.log('validado')
   
       }, 3000);
     }
-
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading]); 
+  }, [state.loading]); 
 
-  return (
-    <div>
-      <h2>Eliminar {name} </h2>
-      <p>Por favor, escriba el codigo de seguridad para eliminar </p>
-      {loading && (
-        <p>Cargando ...</p>
-      )}
-      <input onChange={onChange} value={value} placeholder='Codigo de seguridad' />
-      <button type='button' onClick={handleComprobar}> Comprobar</button>
-      {(error && !loading) && (
-        <p className='Use-error'>Error: el codigo es incorrecto</p>
-      )}
-    </div>
-  );
+
+  // Ejemplo de estado declarativo con muchas pantallas :)
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name} </h2>
+        <p>Por favor, escriba el codigo de seguridad para eliminar </p>
+        {state.loading && (
+          <p>Cargando ...</p>
+        )}
+        <input 
+          onChange={onChange} 
+          value={state.value} 
+          placeholder='Codigo de seguridad'
+        />
+        <button 
+          type='button' 
+          onClick={handleComprobar}
+          > Comprobar
+        </button>
+  
+        {(state.error && !state.loading) && (
+          <p className='Use-error'>Error: el codigo es incorrecto</p>
+        )}
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <div>
+          <h2>Eliminar {name} </h2>
+          <p>Seguro que quieres eliminar {name}</p>
+          <button
+            onClick={() => {
+              setState({...state, 
+                confirmed: true, 
+                deleted: true
+              })
+            }}>Si, Eliminar</button>
+          <button
+            onClick={() => {
+             setState({
+              ...state, 
+              confirmed: false, 
+              value:''
+            })
+            }}>No, volver</button>
+        </div>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+      <div>
+        <h2>{name} fue eliminado</h2>
+        <button
+         onClick={()=>{
+          setState({
+            ...state,
+            confirmed: false, 
+            deleted:false,
+            value:''
+          })
+         }}>Recuperar {name}</button>
+      </div>
+      </React.Fragment>
+    );
+  }
 }
+
 export { UseState };
  
